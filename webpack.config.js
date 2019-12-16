90e82743941957e4cd46fb4eef3b025f8dbbd08b
TpaBoJITa <zhenya-i5@mail.ru>
2019-12-16 13:25:16 +0600

    add sass compile settings

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         },
       },
       {
-        test: /\.scss$/,
+        test: /\.(scss|sass)$/,
         use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       }
     ]
